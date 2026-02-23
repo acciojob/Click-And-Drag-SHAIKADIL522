22ee2cd99c0c399de701324dd886086deb14a125
ADIL <adilshaik9112@gmail.com>
2026-02-23 08:58:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const slider = document.querySelector('.items');
+
 let isDown = false;
-let startX;
-let scrollLeft;
+let startX = 0;
+let startScrollLeft = 0;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+  startX = e.pageX;
+  startScrollLeft = slider.scrollLeft;
 });
 
-document.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-
-document.addEventListener('mousemove', (e) => {
+slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
   e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX);
-  slider.scrollLeft = scrollLeft - walk;
+  const x = e.pageX;
+  const walk = startX - x;
+  slider.scrollLeft = startScrollLeft + walk;
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
 });
 
 slider.addEventListener('mouseleave', () => {
